cab1696245e52137ff7af304b7b509ac39116736
DirectorOfUnskillful <101033868+DirectorOfUnskillful@users.noreply.github.com>
2023-09-05 19:56:43 +0530

    Update Q2_printnostring.py

--- Q2_printnostring.py
+++ Q2_printnostring.py
@@ -1,4 +1,4 @@
-print("""
+
 Q2) Given a list of strings, count and print the number of strings where the string length is 
 2 or more & the 1st & last characters are same.
 
@@ -8,6 +8,6 @@ def count_strings(list):
     if len(s) >=2 and s[0] == s[-1]:
       count+=1
   return count
-strings = ["racecar", "apple", "civic", "pyhton", "madam"]
+strings = ["racecar", "apple", "civic", "python", "madam"]
 count = count_strings(strings)
-print(f" there are {count} strings the meet the criteria")""")
+print(f" there are {count} strings the meet the criteria")
